b6c282a4d3e696f3a7efdf33382c3e7647fc5b4c
Legolas Bloom <zhanhsw@gmail.com>
2020-03-26 21:46:46 +0800

    leetcode: add 177. Nth Highest Salary

--- leetcode-database.sql
+++ leetcode-database.sql
@@ -1,10 +1,25 @@
--- 175. Combine Two Tables
+# 175. Combine Two Tables
 SELECT p.FirstName, p.LastName, a.City, a.State FROM Person AS p
 Left JOIN Address AS a ON p.PersonId = a.PersonId;
 
--- 176. Second Highest Salary
+# 176. Second Highest Salary
 SELECT (
     SELECT DISTINCT Salary FROM Employee
     ORDER BY Salary DESC
     LIMIT 1 OFFSET 1
-) AS SecondHighestSalary; -- 外层select as 解决 null问题
+) AS SecondHighestSalary;  -- 外层select as 解决 null问题
+
+# 177. Nth Highest Salary
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  # https://www.mysqltutorial.org/variables-in-stored-procedures.aspx
+  DECLARE K INT DEFAULT N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT (
+          SELECT DISTINCT Salary FROM Employee
+          ORDER BY Salary DESC
+          LIMIT 1 OFFSET K
+      ) AS SecondHighestSalary
+  );
+END
