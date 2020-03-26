61638cf17a2a4965c36521ed13038d06fcf274bf
Legolas Bloom <zhanhsw@gmail.com>
2020-03-26 21:31:34 +0800

    leetcode: add 176. Second Highest Salary

--- leetcode-database.sql
+++ leetcode-database.sql
@@ -1,3 +1,10 @@
-# 175. Combine Two Tables
+-- 175. Combine Two Tables
 SELECT p.FirstName, p.LastName, a.City, a.State FROM Person AS p
 Left JOIN Address AS a ON p.PersonId = a.PersonId;
+
+-- 176. Second Highest Salary
+SELECT (
+    SELECT DISTINCT Salary FROM Employee
+    ORDER BY Salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary; -- 外层select as 解决 null问题
