c22e1b32121c2a29a8304d8496f964e8d399fbff
Legolas Bloom <zhanhsw@gmail.com>
2020-03-26 22:15:00 +0800

    leetcode: add 178. Rank Scores

--- leetcode-database.sql
+++ leetcode-database.sql
@@ -23,3 +23,22 @@ BEGIN
       ) AS SecondHighestSalary
   );
 END
+
+# 178. Rank Scores
+SELECT Score, (
+        SELECT COUNT(DISTINCT Score) FROM Scores
+        WHERE Score >= S.Score
+    ) AS Rank
+FROM Scores AS S
+ORDER BY Score DESC; -- 568 ms
+SELECT Score, (
+        SELECT COUNT(*) FROM (
+            SELECT DISTINCT Score AS s FROM Scores
+        ) AS t WHERE s >= Score
+    ) Rank
+FROM Scores ORDER BY Score DESC; -- 207 ms
+SELECT s.Score, COUNT(DISTINCT t.Score) AS Rank
+FROM Scores s
+JOIN Scores t ON s.Score <= t.Score
+GROUP BY s.Id
+ORDER BY s.Score DESC; -- 639 ms
