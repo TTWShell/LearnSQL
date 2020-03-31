bb1f61ff77884225cc6639e54bd4143bc31cb800
Legolas Bloom <zhanhsw@gmail.com>
2020-03-31 10:21:53 +0800

    leetcode: add # 180. Consecutive Numbers

--- leetcode-database.sql
+++ leetcode-database.sql
@@ -31,14 +31,34 @@ SELECT Score, (
     ) AS Rank
 FROM Scores AS S
 ORDER BY Score DESC; -- 568 ms
+
 SELECT Score, (
         SELECT COUNT(*) FROM (
             SELECT DISTINCT Score AS s FROM Scores
         ) AS t WHERE s >= Score
     ) Rank
 FROM Scores ORDER BY Score DESC; -- 207 ms
+
 SELECT s.Score, COUNT(DISTINCT t.Score) AS Rank
 FROM Scores s
 JOIN Scores t ON s.Score <= t.Score
 GROUP BY s.Id
 ORDER BY s.Score DESC; -- 639 ms
+
+# 180. Consecutive Numbers
+SELECT DISTINCT a.Num AS ConsecutiveNums FROM Logs a
+JOIN logs b
+JOIN logs c
+WHERE
+    a.Id = b.Id-1 AND b.Id = c.Id-1 AND
+    a.Num = b.Num AND b.Num = c.Num;
+
+SELECT DISTINCT Num AS ConsecutiveNums
+FROM (
+    SELECT *, @cnt := equal_last * @cnt + 1 AS cnt
+        FROM (
+            SELECT Num, @prev = (@prev := Num
+        ) AS equal_last
+    FROM Logs, (SELECT @prev := -1) init) tmp, (SELECT @cnt := 0) init1
+) AS tmp1
+WHERE cnt > 2;
